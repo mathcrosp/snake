347d5acb7735719955323271991c4426b80de2a6
Dmitry Zubarev <mathcrosp@yandex.ru>
2017-05-31 01:27:46 +0500

    Fix indent.

--- snake.asm
+++ snake.asm
@@ -5,30 +5,30 @@ locals
 
 .data
 
-    max_len         equ  30
-    curr_len        dw  8
+    max_len     equ  30
+    curr_len    dw  8
 
     start_head_x    equ  100
     start_head_y    equ  120
 
-    snake_xs        dw  max_len*2 dup(start_head_x)
-    snake_ys        dw  max_len*2 dup(start_head_y)
+    snake_xs    dw  max_len*2 dup(start_head_x)
+    snake_ys    dw  max_len*2 dup(start_head_y)
 
-    head_x          dw  ?
-    head_y          dw  ?
+    head_x      dw  ?
+    head_y      dw  ?
 
-    tail_x          dw  ?
-    tail_y          dw  ?
+    tail_x      dw  ?
+    tail_y      dw  ?
 
-    head_dx         dw  1
-    head_dy         dw  0
+    head_dx     dw  1
+    head_dy     dw  0
 
     cell_size       dw  10
 
     snake_color     db  14
     wall_color      db  4
 
-    delay           dd  400000
+    delay       dd  400000
     min_delay       dd  49000
     max_delay       dd  1200000
 
@@ -38,7 +38,7 @@ locals
 
 .code
 
-	org 100h
+    org 100h
 
 start:
     jmp     main
@@ -74,18 +74,18 @@ main_loop endp
 
 init_snake proc near
 
-    mov		head_x, start_head_x
-    mov		head_y, start_head_y
-    mov		bx, 0
+    mov    head_x, start_head_x
+    mov    head_y, start_head_y
+    mov    bx, 0
 
 @@fill:
-    cmp		bx, curr_len
-    je		@@exit
-    mov		dx, snake_ys[bx]
-    add		dx, [cell_size]
-    add		bx, 2
-    mov		snake_ys[bx], dx
-    jmp		@@fill
+    cmp    bx, curr_len
+    je     @@exit
+    mov    dx, snake_ys[bx]
+    add    dx, [cell_size]
+    add    bx, 2
+    mov    snake_ys[bx], dx
+    jmp    @@fill
 
 @@exit:
     ret
@@ -183,27 +183,27 @@ update_head endp
 
 update_array proc near
 
-    mov     bx, curr_len
+    mov    bx, curr_len
 
 @@filling_loop:
-    cmp		bx, 0
-    je	   	@@write_head
-    sub		bx, 2
-    mov		cx, snake_xs[bx]
-    mov		dx, snake_ys[bx]
-    add		bx, 2
-    mov		snake_xs[bx], cx
-    mov		snake_ys[bx], dx
-    sub		bx, 2
-    jmp		@@filling_loop
+    cmp     bx, 0
+    je      @@write_head
+    sub     bx, 2
+    mov     cx, snake_xs[bx]
+    mov     dx, snake_ys[bx]
+    add     bx, 2
+    mov     snake_xs[bx], cx
+    mov     snake_ys[bx], dx
+    sub     bx, 2
+    jmp     @@filling_loop
 
 @@write_head:
     mov     cx, head_x
     mov     dx, head_y
 
     mov     bx, 0
-    mov		snake_xs[bx], cx
-    mov		snake_ys[bx], dx
+    mov     snake_xs[bx], cx
+    mov     snake_ys[bx], dx
 
     ret
 
@@ -238,25 +238,25 @@ draw_snake proc near
     push    cx
     push    dx
 
-    mov		bx, 0
+    mov     bx, 0
 
 @@draw_loop:
-    cmp		bx, [curr_len]
-    je		@@exit
-    mov		cx, snake_xs[bx]
-    mov		dx, snake_ys[bx]
-    push	bx
+    cmp     bx, [curr_len]
+    je      @@exit
+    mov     cx, snake_xs[bx]
+    mov     dx, snake_ys[bx]
+    push    bx
     mov     al, snake_color
-    call	draw_cell
-    pop		bx
-    add		bx, 2
-    jmp 	@@draw_loop
+    call    draw_cell
+    pop     bx
+    add     bx, 2
+    jmp     @@draw_loop
 
 @@exit:
-    mov		cx, snake_xs[0]
-    mov		dx, snake_ys[0]
+    mov     cx, snake_xs[0]
+    mov     dx, snake_ys[0]
     mov     al, snake_color
-    call	draw_cell
+    call    draw_cell
 
     pop     dx
     pop     cx
@@ -324,20 +324,20 @@ game_over proc near
     lea     bp, game_over_msg
     mov     cx, 9
 
-    mov		ah, 0fh
-    int		10h
+    mov     ah, 0fh
+    int     10h
 
-    sub		ah, cl
-    shr		ax, 8
-    mov		bl, 2
-    div		bl
-    mov		dl, al
+    sub     ah, cl
+    shr     ax, 8
+    mov     bl, 2
+    div     bl
+    mov     dl, al
 
-    mov		bh, curr_page
-    mov		bl, snake_color
-    mov		al, 1
-    mov		ah, 13h
-    int 	10h
+    mov     bh, curr_page
+    mov     bl, snake_color
+    mov     al, 1
+    mov     ah, 13h
+    int     10h
 
     call    game_over_beep
 
@@ -353,10 +353,10 @@ game_over endp
 
 wait proc near
 
-    mov		ax, 8600h
-    mov		cx, word ptr delay+2
-    mov		dx, word ptr delay
-    int		15h
+    mov     ax, 8600h
+    mov     cx, word ptr delay+2
+    mov     dx, word ptr delay
+    int     15h
 
     ret
 
